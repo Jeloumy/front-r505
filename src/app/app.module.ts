e0dc2139909d9a48afe427588f18b4a2518f1ec7
DJeremy <96179359+Jeloumy@users.noreply.github.com>
2023-11-08 18:01:32 +0100

    refactor : création de la route inscription

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
+import { AccueilComponent } from './pages/accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InscriptionComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
