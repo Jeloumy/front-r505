0ce8c6187cae9587a584b1a448ad0da34ec41278
DJeremy <96179359+Jeloumy@users.noreply.github.com>
2023-11-09 16:37:55 +0100

    feat : modification du fichier .ts/ajout de la fonction submitInscription

--- src/app/pages/inscription/inscription.component.ts
+++ src/app/pages/inscription/inscription.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
-  styleUrls: ['./inscription.component.scss']
+  styleUrls: ['./inscription.component.scss'],
 })
-export class InscriptionComponent {
 
+
+export class InscriptionComponent {
+  userData = {
+    name: '',
+    email: '',
+    pseudo: '',
+    password: ''
+  };
+
+  constructor(private authService: AuthService) {}
+
+  submitInscription() {
+
+    this.authService.inscription(this.userData).then(
+      (response) => {
+
+        console.log('Inscription réussie', response);
+      },
+      (error) => {
+
+        console.error('Erreur lors de l inscription', error);
+      }
+    );
+  }
 }
+
