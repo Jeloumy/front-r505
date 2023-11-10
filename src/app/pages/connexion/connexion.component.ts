7a7f72b9ee62f1a93800ea64c678846c4e3ee710
DJeremy <96179359+Jeloumy@users.noreply.github.com>
2023-11-10 09:51:44 +0100

    feat : modification de l'html + modification du ts
    Ajout de la classe ConnexionComponent et la fonction onLoginSubmit

--- src/app/pages/connexion/connexion.component.ts
+++ src/app/pages/connexion/connexion.component.ts
@@ -1,10 +1,29 @@
+// connexion.component.ts
+
 import { Component } from '@angular/core';
+import { AuthService } from './path-to-your-auth.service';
 
 @Component({
   selector: 'app-connexion',
   templateUrl: './connexion.component.html',
-  styleUrls: ['./connexion.component.scss']
+  styleUrls: ['./connexion.component.css']
 })
 export class ConnexionComponent {
+  loginData = {
+    identifier: '',  // identifier peut être soit l'email soit le pseudo
+    password: ''
+  };
 
+  constructor(private authService: AuthService) {}
+
+  onLoginSubmit() {
+    this.authService.login(this.loginData).then((response) => {
+      // Traiter la réponse, gérer le token, rediriger vers la page d'accueil, etc.
+      console.log(response);
+    }).catch((error) => {
+      // Gérer les erreurs, afficher un message d'erreur, etc.
+      console.error(error);
+    });
+  }
 }
+
