d9b16ea32f32c097049ad80e4b1b095cb80b7dd7
DJeremy <96179359+Jeloumy@users.noreply.github.com>
2023-11-23 14:04:26 +0100

    feat : ajout des fonctions (récupération des données + requêtes API))

--- src/app/pages/tournois/tournois.component.ts
+++ src/app/pages/tournois/tournois.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../shared/services/api.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-tournois',
   templateUrl: './tournois.component.html',
-  styleUrls: ['./tournois.component.scss']
+  styleUrls: ['./tournois.component.scss'],
 })
-export class TournoisComponent {
 
+export class TournoisComponent {
+  TournoiForm: FormGroup;
+
+  constructor(private apiService: ApiService, private router: Router, private fb: FormBuilder) {
+    this.TournoiForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      begin_date: ['', Validators.required],
+      end_date: ['', Validators.required],
+      jeu_id: ['', Validators.required],
+    });
+  }
+
+  submitTournoi() {
+    console.log('Début de la fonction submitInscription');
+    if (this.TournoiForm.valid) {
+      console.log('Formulaire valide');
+      const formData = this.TournoiForm.value;
+
+      this.apiService.tournoi(formData).then(
+        (response) => {
+          console.log('création tournoi réussie', response);
+
+          this.router.navigate(['']);
+        },
+        (error) => {
+          console.error('Erreur lors de l\'tournoi', error);
+        }
+      );
+    }
+  }
 }
