7256181e5745f13a15f966ac42c9b20f89df49ff
DJeremy <96179359+Jeloumy@users.noreply.github.com>
2024-01-12 01:36:46 +0100

    feat : ajout du component tournoi-details

---
+++ src/app/pages/tournoi-details/tournoi-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tournoi-details',
+  templateUrl: './tournoi-details.component.html',
+  styleUrls: ['./tournoi-details.component.scss']
+})
+export class TournoiDetailsComponent {
+
+}
