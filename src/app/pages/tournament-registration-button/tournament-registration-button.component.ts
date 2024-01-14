e35a69361da2caa75a33da145c5c81e43db0fe9c
DJeremy <96179359+Jeloumy@users.noreply.github.com>
2024-01-14 03:06:35 +0100

    feat : modification html + ts

--- src/app/pages/tournament-registration-button/tournament-registration-button.component.ts
+++ src/app/pages/tournament-registration-button/tournament-registration-button.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ApiService } from 'chemin-vers-ton-service/api.service';
 
 @Component({
   selector: 'app-tournament-registration-button',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./tournament-registration-button.component.scss']
 })
 export class TournamentRegistrationButtonComponent {
+  @Input() tournamentId: number;
+  @Input() isRegistered: boolean;
 
+  constructor(private apiService: ApiService) {}
+
+  toggleRegistration() {
+    if (this.isRegistered) {
+      this.leaveTournament();
+    } else {
+      this.registerToTournament();
+    }
+  }
+
+  registerToTournament() {
+    // Appelle la méthode du service pour s'inscrire au tournoi
+    this.apiService.addTeamToTournament(this.tournamentId).subscribe(
+      () => {
+        // Mise à jour de l'état d'inscription
+        this.isRegistered = true;
+      },
+      (error) => {
+        // Gestion des erreurs
+        console.error(error);
+      }
+    );
+  }
+
+  leaveTournament() {
+    // Appelle la méthode du service pour se désinscrire du tournoi
+    this.apiService.leaveTournament(this.tournamentId).subscribe(
+      () => {
+        // Mise à jour de l'état d'inscription
+        this.isRegistered = false;
+      },
+      (error) => {
+        // Gestion des erreurs
+        console.error(error);
+      }
+    );
+  }
 }
+
